9f167b94bb132d5211eadf1a15e0824f2e37313e
Michael Ashby <ashby.michael@gmail.com>
2015-12-15 20:03:05 -0800

    write user model tests and pass them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true, length: {in: 3..16}
+  validates :password, presence: true, length: {minimum: 6}
 end
