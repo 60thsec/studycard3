d5815797628ec089a5df7fe8327b649c5c61b8ea
Michael Ashby <ashby.michael@gmail.com>
2015-12-18 18:50:30 -0800

    add font-awesome, pure css, and set up splash page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    if logged_in?
+      redirect to decks_url
+    else
+      render :index, layout: 'splash'
+    end
+  end
+
   private
     def auth_log_in(user)
       session[:user_id] = user.id
       flash[:notice] = "Hello, #{user.username}"
+    end
+
+    def logged_in?
+      return true if session[:user_id]
     end
 
     def get_current_user
