5d94204f9048decd09306bd550c87cccf578555f
Michael Ashby <ashby.michael@gmail.com>
2016-05-01 18:00:16 -0700

    add api user routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
     resources :cards
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+
   get    '/login'      => 'sessions#new'
   delete '/logout'     => 'sessions#destroy'
   post   '/study/:id'  => 'decks#study'
