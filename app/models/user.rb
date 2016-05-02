b8b39ee448befd266fd9908d450bafcf5b474f38
Michael Ashby <ashby.michael@gmail.com>
2016-05-01 18:40:21 -0700

    validate unique auth_tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :decks 
+  has_many :decks
   validates :username, presence: true, uniqueness: true, length: {in: 3..16}
   validates :password, presence: true, length: {minimum: 6}
+  validates :auth_token, uniqueness: true
 end
